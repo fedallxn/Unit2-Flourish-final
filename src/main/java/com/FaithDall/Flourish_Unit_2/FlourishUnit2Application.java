f535d1679751ccba51bdcaf5a4f057dac8aa209a
fedallxn <faithe.dall@gmail.com>
2026-08-26 19:06:08 -0500

    Initial commit

---
+++ src/main/java/com/FaithDall/Flourish_Unit_2/FlourishUnit2Application.java
@@ -0,0 +1,13 @@
+package com.FaithDall.Flourish_Unit_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlourishUnit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlourishUnit2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/FaithDall/Flourish_Unit_2/FlourishUnit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FaithDall.Flourish_Unit_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlourishUnit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
