74d57d4262b50d053103ebc39bc4f0ec29973454
fedallxn <faithe.dall@gmail.com>
2026-09-15 20:06:03 -0500

    Created a SpeciesController with a custom query in the repository to search the database by commonName rather than an Id. Species controller has a get mapping to return the species information by common name. A post mapping to add a new species, and a put mapping to edit species...this is mainly for the developer if they need to change anything, not necessarily for the user.

---
+++ src/main/java/com/FaithDall/Flourish_Unit_2/controllers/SpeciesController.java
@@ -0,0 +1,33 @@
+package com.FaithDall.Flourish_Unit_2.controllers;
+
+import com.FaithDall.Flourish_Unit_2.models.Species;
+import com.FaithDall.Flourish_Unit_2.repositories.SpeciesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("speciesInfo")
+public class SpeciesController {
+    @Autowired
+    private SpeciesRepository speciesRepository;
+
+    @GetMapping("{commonName}")
+    public Species getSpeciesInfo(@PathVariable String commonName) {
+        return speciesRepository.findByCommonName(commonName);
+    }
+
+    @PostMapping
+    public Species addSpecies(@RequestBody Species newSpecies) {
+        return speciesRepository.save(newSpecies);
+    }
+
+    @PutMapping("{speciesId}")
+    public Species updateSpeciesInfo(@PathVariable int speciesId, @RequestBody Species updatedSpecies) {
+        Species currentSpecies = speciesRepository.findById(speciesId).orElse(null);
+        currentSpecies.setWateringFrequency(updatedSpecies.getWateringFrequency());
+        currentSpecies.setLightRequirement(updatedSpecies.getLightRequirement());
+        currentSpecies.setCareInfo(updatedSpecies.getCareInfo());
+        currentSpecies.setPlantImageURL(updatedSpecies.getPlantImageURL());
+        return speciesRepository.save(currentSpecies);
+    }
+}
--- src/main/java/com/FaithDall/Flourish_Unit_2/repositories/SpeciesRepository.java
+++ src/main/java/com/FaithDall/Flourish_Unit_2/repositories/SpeciesRepository.java
@@ -1,4 +1,9 @@
 package com.FaithDall.Flourish_Unit_2.repositories;
 
-public interface SpeciesRepository {
+import com.FaithDall.Flourish_Unit_2.models.Species;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpeciesRepository extends JpaRepository<Species, Integer> {
+    //don't need a list because it's returning ONE species that matches the commonName
+    Species findByCommonName(String commonName);
 }
